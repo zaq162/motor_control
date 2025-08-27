d648f94da2adda8400419e52373eca46a81b0a2c
zaq162 <2148441563@qq.com>
2025-08-27 09:56:05 +0800

    2025.8.27

---
+++ zhukongban3.1test/zhukongban3/Core/Src/main.c
@@ -0,0 +1,478 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "can.h"
+#include "dma.h"
+#include "i2c.h"
+#include "rtc.h"
+#include "tim.h"
+#include "usart.h"
+#include "usb_otg.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "hardware.h"
+#include "stm32f4xx.h" 
+uint8_t rx1_buffer[RX_BUFFER_SIZE];   //接电机，串口1 485
+uint8_t rx4_buffer[RX_BUFFER_SIZE];   //接高压板，串口4  232
+uint8_t rx3_buffer[RX_BUFFER_SIZE];   //接上位机，串口3 422
+uint8_t tim2_buffer[5];
+uint8_t fault_buffer[11]= {0x5A, 0x01, 0x08, 0x66, 0x66,
+                       0x00, 0x00, 0x01, 0x00, 0x00, 0x55};
+
+uint16_t rx1_length;
+uint16_t rx4_length;
+uint16_t rx3_length;
+uint16_t tim2_length;
+
+uint8_t  rx1_flag = 0;
+uint8_t  rx4_flag = 0;
+uint8_t  rx3_flag = 0;
+
+uint8_t motor_flag = 0;     //如果等于0则对应信号转高压板
+uint8_t com_flag = 0;       //如果等于1则对应信号转高压板
+
+uint16_t k =0;              
+uint8_t expose_flag = 0;    //曝光标志位
+uint8_t pre_flag = 0;       //预备标志位
+uint8_t Gybpre_flag =0;     //高压板是否准备
+uint8_t Syn_flag =0;        //同步标志位
+uint8_t over_flag = 0;      //结束标志位，没用到
+uint8_t fault_flag=0;       //高压板报错
+uint8_t motor_revflag=0;    //电机反转到位
+uint8_t detector_flag=0;    //探测器的连接、参数设置之后点“开始采集按钮”
+uint8_t KV85_1=1; 
+
+uint16_t n_total;       //拍照总张数，从上位机读取
+uint16_t t_time;        //周期,帧率倒数  
+uint8_t  pose_time;     //曝光时间
+uint8_t  state = 0;     //用于判断进度，初始是0，pre后为1，expose结束后返回0
+uint8_t stop_flag = 0;  //没用到
+
+uint32_t pos = 0; // 编码器计数值
+uint8_t diretion=0;//编码器方向,逆时针是01，顺时针是00
+
+unsigned char SynState = 0;   //同步
+unsigned char ExpState = 0;   //曝光
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_CAN1_Init();
+  MX_CAN2_Init();
+  MX_I2C3_Init();
+  MX_USART1_UART_Init();
+  MX_USART3_UART_Init();
+  MX_USB_OTG_FS_HCD_Init();
+  MX_RTC_Init();
+  MX_TIM2_Init();
+  MX_TIM3_Init();
+  MX_UART4_Init();
+  MX_TIM4_Init();
+  /* USER CODE BEGIN 2 */
+
+//	HAL_UART_Receive_IT(&huart1,rx_buffer ,sizeof(rx_buffer));  //中断接收
+
+// 使能空闲中断
+  __HAL_UART_ENABLE_IT(&huart1, UART_IT_IDLE);
+  __HAL_UART_ENABLE_IT(&huart4, UART_IT_IDLE);
+	__HAL_UART_ENABLE_IT(&huart3, UART_IT_IDLE);
+
+//  // 启动DMA接收
+  HAL_UART_Receive_DMA(&huart1, rx1_buffer, sizeof(rx1_buffer));
+	HAL_UART_Receive_DMA(&huart4, rx4_buffer, sizeof(rx4_buffer));
+	HAL_UART_Receive_DMA(&huart3, rx3_buffer, sizeof(rx3_buffer));
+
+
+
+	HAL_TIM_Encoder_Start(&htim2, TIM_CHANNEL_ALL);   // 一次启动所有通道
+	__HAL_TIM_SET_COUNTER(&htim2,0);  //清零定时器2的计数值
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+	
+	
+	t_time  = 1/20*1000 ;    //20帧用的毫秒数      
+	pose_time = t_time /2 ;  //曝光时间占一半      
+	
+  while (1)
+  {
+		
+		if(rx3_flag ==1)  // 上位机下发数据
+		{
+			rx3_flag = 0;
+			if(rx3_buffer[0] == 0x5A)  //除电机外的数据
+			{
+				GybMessage message_422 = 
+				{
+					.Header = rx3_buffer[0], //数据帧头
+					.Addr = rx3_buffer[1],  // 数据地址
+					.Len = rx3_buffer[2],   // 数据长度
+					.Byte0 = rx3_buffer[3], // 指令
+					.Byte1 = rx3_buffer[4], // 特殊位索引1
+					.Byte2 = rx3_buffer[5], // 特殊位索引 2
+					.Byte3 = rx3_buffer[6], // 特殊位索引 3
+					.Byte4 = rx3_buffer[7], // 数据字节 1
+					.Byte5 = rx3_buffer[8], // 数据字节 2
+					.Byte6 = rx3_buffer[9], // 数据字节 3
+					.Byte7 = rx3_buffer[10] // 数据字节 4
+				};
+				
+				if(message_422.Byte0 == 0x21 && message_422.Byte4 == 0x01)   //上位机手闸一
+				{
+					pre_flag = 1;
+				}
+				else if(message_422.Byte0 == 0x22 && message_422.Byte4 == 0x01)  //上位机手闸二
+				{
+					expose_flag = 1;
+				}
+				else if(message_422.Byte0 == 0x55 && message_422.Byte1 == 0x66)  //松开电机刹车线
+				{
+					HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_RESET);  //低电平有效
+				}
+				else if(message_422.Byte0 == 0x55 && message_422.Byte1 == 0x55)  //电机反转
+				{
+					motor_revflag = 1;
+				}
+				else if(message_422.Byte0 == 0x77 && message_422.Byte1 == 0x77)  //在探测器的连接、参数设置之后点“开始采集按钮”
+				{
+					detector_flag = 1;
+				}
+				else if(message_422.Byte0 == 0x23 && message_422.Byte4 == 0x01)   //上位机停止
+				{
+					stop_flag = 1;
+				}
+				else if(message_422.Byte0 == 0x27)         //拍照总张数
+				{
+					n_total = message_422.Byte4*256 + message_422.Byte5;
+				}
+				else if(message_422.Byte0 == 0x15)         //脉冲曝光周期 T
+				{
+					t_time = message_422.Byte3*256 + message_422.Byte4;  //t_time它的单位是0.1ms,
+					
+          __HAL_TIM_SET_AUTORELOAD(&htim4, t_time/2-1);  // 修改 TIM4 的自动重装载值（ARR）
+         // __HAL_TIM_SET_PRESCALER(&htim2, new_psc);   // 修改 TIM4 的和预分频（PSC）
+          HAL_TIM_GenerateEvent(&htim2, TIM_EVENTSOURCE_UPDATE);  //立即生效
+				}
+				else if(message_422.Byte0 == 0x16)         //曝光时间 T1
+				{
+					pose_time = message_422.Byte3*256 + message_422.Byte4;
+				 __HAL_TIM_SET_AUTORELOAD(&htim3, pose_time-1);  // 修改 TIM4 的自动重装载值（ARR）
+					 HAL_TIM_GenerateEvent(&htim2, TIM_EVENTSOURCE_UPDATE); 
+				}
+				else
+				{
+					com_flag = 1;   
+					HAL_UART_Transmit_DMA(&huart4, rx3_buffer, rx3_length);   //转发给高压板
+				}	
+			}
+		
+			else if(rx3_buffer[0] == 0x01)  	//增加一个判断标志，判断是否是发送给电机的数据
+			{
+				motor_flag =1;
+			 
+				HAL_UART_Transmit_DMA(&huart1, rx3_buffer, rx3_length);   //转发给电机
+			}
+		}
+		
+	 
+		// 查询电机位置，但是读的是定时器的计数和电机无关
+    pos = __HAL_TIM_GET_COUNTER(&htim2);
+		diretion=__HAL_TIM_IS_TIM_COUNTING_DOWN(&htim2);
+		tim2_buffer[0] =diretion;
+		tim2_buffer[1] = (uint8_t)(pos & 0xFF);
+    tim2_buffer[2] = (uint8_t)((pos >> 8) & 0xFF);
+    tim2_buffer[3] = (uint8_t)((pos >> 16) & 0xFF);
+    tim2_buffer[4] = (uint8_t)((pos >> 24) & 0xFF);
+    tim2_length = 5;
+		HAL_UART_Transmit(&huart3, tim2_buffer, tim2_length ,0xFFFF );
+		//testtesttesttesttesttesttest
+/*pwm	
+		n_total=5000;
+		HAL_TIM_Base_Start_IT(&htim4);  //启动计时器4计数，产生PWM
+		 while (1) {
+     //计数等待
+			__NOP(); // 空指令避免编译器优化
+    } 
+		
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_RESET);   //刹车线
+		*/
+		//testtesttesttesttesttesttest
+		
+		
+		if (pre_flag == 1)  //判断是否来了准备信号
+		{
+			HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,GPIO_PIN_SET); //IO口通知高压板准备
+			pre_flag = 0;	
+			
+	  }
+		
+		if( Gybpre_flag == 1 && motor_revflag == 1 && detector_flag == 1)  //判断电机是否反转好&&高压板是否准备好&&探测器准备好
+		{
+			state = 1; //状态机	
+			Gybpre_flag=0;
+			motor_revflag=0;
+			detector_flag=0;
+		}
+		
+		if (state ==1 && expose_flag ==1)  //state代表一切就绪，expose代表发出pwm(手闸二)
+			{ 
+				HAL_TIM_Base_Start_IT(&htim4);  //启动计时器4计数，产生PWM
+				expose_flag =0;
+	  	}
+
+		
+		if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_4) == GPIO_PIN_RESET && KV85_1==1 )  //高压板的KV85,下降沿有效
+		{
+			KV85_1=0;
+			HAL_UART_Transmit(&huart3, fault_buffer, 11 ,0xFFFF );
+		}
+		else if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_4) == GPIO_PIN_SET)
+		{
+		KV85_1=1;
+		}
+			
+		
+		HAL_Delay(500);   
+
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSI|RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 72;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 3;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+//外部中断回调函数，其实依然可以使用查询方式读取信号状态,目前中断过多，可以删除
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)  
+{  
+	if(GPIO_Pin == GPIO_PIN_14)  //探测器返回信号FPD;PE14
+   {
+		Syn_flag = 1;
+	 // __HAL_TIM_SET_COUNTER(&htim3,0);  //清零定时器3的计数值
+		 HAL_TIM_Base_Start_IT(&htim3);
+		 HAL_GPIO_WritePin(GPIOC, GPIO_PIN_4, GPIO_PIN_RESET);   //拉高曝光引脚COM ,PC4
+	 }
+	 
+	 //增加一个判断
+	 if(GPIO_Pin == GPIO_PIN_4)   //手闸一；PE4
+   {
+		pre_flag = 1;
+	 }	 
+	 
+	 if(GPIO_Pin == GPIO_PIN_2)   //手闸二;PE2
+   {
+		expose_flag = 1;
+	 }
+	 
+	 if(GPIO_Pin == GPIO_PIN_5)   //高压板报错，Fault。PA5
+   {
+		fault_flag = 1;
+		 //急听操作
+		 
+		  HAL_TIM_Base_Stop_IT(&htim4);  	//  停止发PWM
+		 
+			DataFrame_Motor1 frame1 = 
+			{
+					.Header = 0x01,
+					.Cmd = 0x05,
+					.addr = 0x0123,
+					.data = 0xFF00
+				};
+			
+			DataFrame_Motor1 *p1 = &frame1;
+			uint8_t data[8];
+			Send_motor1(p1, data);//计算校验位CRC
+			HAL_UART_Transmit_DMA(&huart1, data, sizeof(data));  //电机急停止
+				
+	 }
+	 
+	 if(GPIO_Pin == GPIO_PIN_6)  //高压板回准备信号;PA6下降沿触发
+   {
+		Gybpre_flag = 1;
+	 }
+	 
+ }
+
+ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)  //定时器中断回调函数
+{
+		if (htim->Instance == TIM3)  
+    {
+				HAL_GPIO_WritePin(GPIOC, GPIO_PIN_4, GPIO_PIN_RESET);   //拉低曝光引脚COM ,PC4
+        HAL_TIM_Base_Stop_IT(&htim3);  	//  停止定时器（避免计数干扰）
+    }
+		
+		if (htim->Instance == TIM4)  
+    {
+			if(SynState < 2 * n_total)
+			{
+				HAL_GPIO_TogglePin(GPIOE, GPIO_PIN_15);    //SYNC_InP,启动同步
+				SynState ++;
+			}
+			else
+			{
+				state = 0;
+				SynState=0;
+		    HAL_TIM_Base_Stop_IT(&htim4);  	//  停止定时器（避免计数干扰）
+				HAL_GPIO_WritePin(GPIOA,GPIO_PIN_7,GPIO_PIN_RESET); //IO口通知高压板结束准备
+			}
+    }
+}
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ zhukongban3.1test/zhukongban3/Core/Src/stm32f4xx_it.c
@@ -0,0 +1,510 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    stm32f4xx_it.c
+  * @brief   Interrupt Service Routines.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32f4xx_it.h"
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN TD */
+
+extern uint8_t rx1_buffer[RX_BUFFER_SIZE];
+extern uint8_t rx4_buffer[RX_BUFFER_SIZE];
+extern uint8_t rx3_buffer[RX_BUFFER_SIZE];
+extern uint16_t rx1_length;
+extern uint16_t rx4_length;
+extern uint16_t rx3_length;
+extern uint8_t rx1_flag;
+extern uint8_t rx4_flag;
+extern uint8_t rx3_flag;
+extern uint8_t com_flag;
+extern uint8_t motor_flag;
+
+uint16_t i;
+
+/* USER CODE END TD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/* External variables --------------------------------------------------------*/
+extern TIM_HandleTypeDef htim2;
+extern TIM_HandleTypeDef htim3;
+extern TIM_HandleTypeDef htim4;
+extern DMA_HandleTypeDef hdma_uart4_rx;
+extern DMA_HandleTypeDef hdma_uart4_tx;
+extern DMA_HandleTypeDef hdma_usart1_rx;
+extern DMA_HandleTypeDef hdma_usart1_tx;
+extern DMA_HandleTypeDef hdma_usart3_rx;
+extern DMA_HandleTypeDef hdma_usart3_tx;
+extern UART_HandleTypeDef huart4;
+extern UART_HandleTypeDef huart1;
+extern UART_HandleTypeDef huart3;
+/* USER CODE BEGIN EV */
+
+/* USER CODE END EV */
+
+/******************************************************************************/
+/*           Cortex-M4 Processor Interruption and Exception Handlers          */
+/******************************************************************************/
+/**
+  * @brief This function handles Non maskable interrupt.
+  */
+void NMI_Handler(void)
+{
+  /* USER CODE BEGIN NonMaskableInt_IRQn 0 */
+
+  /* USER CODE END NonMaskableInt_IRQn 0 */
+  /* USER CODE BEGIN NonMaskableInt_IRQn 1 */
+   while (1)
+  {
+  }
+  /* USER CODE END NonMaskableInt_IRQn 1 */
+}
+
+/**
+  * @brief This function handles Hard fault interrupt.
+  */
+void HardFault_Handler(void)
+{
+  /* USER CODE BEGIN HardFault_IRQn 0 */
+
+  /* USER CODE END HardFault_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_HardFault_IRQn 0 */
+    /* USER CODE END W1_HardFault_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles Memory management fault.
+  */
+void MemManage_Handler(void)
+{
+  /* USER CODE BEGIN MemoryManagement_IRQn 0 */
+
+  /* USER CODE END MemoryManagement_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_MemoryManagement_IRQn 0 */
+    /* USER CODE END W1_MemoryManagement_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles Pre-fetch fault, memory access fault.
+  */
+void BusFault_Handler(void)
+{
+  /* USER CODE BEGIN BusFault_IRQn 0 */
+
+  /* USER CODE END BusFault_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_BusFault_IRQn 0 */
+    /* USER CODE END W1_BusFault_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles Undefined instruction or illegal state.
+  */
+void UsageFault_Handler(void)
+{
+  /* USER CODE BEGIN UsageFault_IRQn 0 */
+
+  /* USER CODE END UsageFault_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_UsageFault_IRQn 0 */
+    /* USER CODE END W1_UsageFault_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles System service call via SWI instruction.
+  */
+void SVC_Handler(void)
+{
+  /* USER CODE BEGIN SVCall_IRQn 0 */
+
+  /* USER CODE END SVCall_IRQn 0 */
+  /* USER CODE BEGIN SVCall_IRQn 1 */
+
+  /* USER CODE END SVCall_IRQn 1 */
+}
+
+/**
+  * @brief This function handles Debug monitor.
+  */
+void DebugMon_Handler(void)
+{
+  /* USER CODE BEGIN DebugMonitor_IRQn 0 */
+
+  /* USER CODE END DebugMonitor_IRQn 0 */
+  /* USER CODE BEGIN DebugMonitor_IRQn 1 */
+
+  /* USER CODE END DebugMonitor_IRQn 1 */
+}
+
+/**
+  * @brief This function handles Pendable request for system service.
+  */
+void PendSV_Handler(void)
+{
+  /* USER CODE BEGIN PendSV_IRQn 0 */
+
+  /* USER CODE END PendSV_IRQn 0 */
+  /* USER CODE BEGIN PendSV_IRQn 1 */
+
+  /* USER CODE END PendSV_IRQn 1 */
+}
+
+/**
+  * @brief This function handles System tick timer.
+  */
+void SysTick_Handler(void)
+{
+  /* USER CODE BEGIN SysTick_IRQn 0 */
+
+  /* USER CODE END SysTick_IRQn 0 */
+  HAL_IncTick();
+  /* USER CODE BEGIN SysTick_IRQn 1 */
+
+  /* USER CODE END SysTick_IRQn 1 */
+}
+
+/******************************************************************************/
+/* STM32F4xx Peripheral Interrupt Handlers                                    */
+/* Add here the Interrupt Handlers for the used peripherals.                  */
+/* For the available peripheral interrupt handler names,                      */
+/* please refer to the startup file (startup_stm32f4xx.s).                    */
+/******************************************************************************/
+
+/**
+  * @brief This function handles EXTI line2 interrupt.
+  */
+void EXTI2_IRQHandler(void)
+{
+  /* USER CODE BEGIN EXTI2_IRQn 0 */
+
+  /* USER CODE END EXTI2_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_2);
+  /* USER CODE BEGIN EXTI2_IRQn 1 */
+
+  /* USER CODE END EXTI2_IRQn 1 */
+}
+
+/**
+  * @brief This function handles EXTI line4 interrupt.
+  */
+void EXTI4_IRQHandler(void)
+{
+  /* USER CODE BEGIN EXTI4_IRQn 0 */
+
+  /* USER CODE END EXTI4_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_4);
+  /* USER CODE BEGIN EXTI4_IRQn 1 */
+
+  /* USER CODE END EXTI4_IRQn 1 */
+}
+
+/**
+  * @brief This function handles DMA1 stream1 global interrupt.
+  */
+void DMA1_Stream1_IRQHandler(void)
+{
+  /* USER CODE BEGIN DMA1_Stream1_IRQn 0 */
+
+  /* USER CODE END DMA1_Stream1_IRQn 0 */
+  HAL_DMA_IRQHandler(&hdma_usart3_rx);
+  /* USER CODE BEGIN DMA1_Stream1_IRQn 1 */
+
+  /* USER CODE END DMA1_Stream1_IRQn 1 */
+}
+
+/**
+  * @brief This function handles DMA1 stream2 global interrupt.
+  */
+void DMA1_Stream2_IRQHandler(void)
+{
+  /* USER CODE BEGIN DMA1_Stream2_IRQn 0 */
+
+  /* USER CODE END DMA1_Stream2_IRQn 0 */
+  HAL_DMA_IRQHandler(&hdma_uart4_rx);
+  /* USER CODE BEGIN DMA1_Stream2_IRQn 1 */
+
+  /* USER CODE END DMA1_Stream2_IRQn 1 */
+}
+
+/**
+  * @brief This function handles DMA1 stream3 global interrupt.
+  */
+void DMA1_Stream3_IRQHandler(void)
+{
+  /* USER CODE BEGIN DMA1_Stream3_IRQn 0 */
+
+  /* USER CODE END DMA1_Stream3_IRQn 0 */
+  HAL_DMA_IRQHandler(&hdma_usart3_tx);
+  /* USER CODE BEGIN DMA1_Stream3_IRQn 1 */
+
+  /* USER CODE END DMA1_Stream3_IRQn 1 */
+}
+
+/**
+  * @brief This function handles DMA1 stream4 global interrupt.
+  */
+void DMA1_Stream4_IRQHandler(void)
+{
+  /* USER CODE BEGIN DMA1_Stream4_IRQn 0 */
+
+  /* USER CODE END DMA1_Stream4_IRQn 0 */
+  HAL_DMA_IRQHandler(&hdma_uart4_tx);
+  /* USER CODE BEGIN DMA1_Stream4_IRQn 1 */
+
+  /* USER CODE END DMA1_Stream4_IRQn 1 */
+}
+
+/**
+  * @brief This function handles EXTI line[9:5] interrupts.
+  */
+void EXTI9_5_IRQHandler(void)
+{
+  /* USER CODE BEGIN EXTI9_5_IRQn 0 */
+
+  /* USER CODE END EXTI9_5_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_5);
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_6);
+  /* USER CODE BEGIN EXTI9_5_IRQn 1 */
+
+  /* USER CODE END EXTI9_5_IRQn 1 */
+}
+
+/**
+  * @brief This function handles TIM2 global interrupt.
+  */
+void TIM2_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM2_IRQn 0 */
+
+  /* USER CODE END TIM2_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim2);
+  /* USER CODE BEGIN TIM2_IRQn 1 */
+
+  /* USER CODE END TIM2_IRQn 1 */
+}
+
+/**
+  * @brief This function handles TIM3 global interrupt.
+  */
+void TIM3_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM3_IRQn 0 */
+
+  /* USER CODE END TIM3_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim3);
+  /* USER CODE BEGIN TIM3_IRQn 1 */
+
+  /* USER CODE END TIM3_IRQn 1 */
+}
+
+/**
+  * @brief This function handles TIM4 global interrupt.
+  */
+void TIM4_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM4_IRQn 0 */
+
+  /* USER CODE END TIM4_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim4);
+  /* USER CODE BEGIN TIM4_IRQn 1 */
+
+  /* USER CODE END TIM4_IRQn 1 */
+}
+
+/**
+  * @brief This function handles USART1 global interrupt.
+  */
+void USART1_IRQHandler(void)
+{
+  /* USER CODE BEGIN USART1_IRQn 0 */
+	  if(__HAL_UART_GET_FLAG(&huart1, UART_FLAG_IDLE) != RESET)
+  {
+    __HAL_UART_CLEAR_IDLEFLAG(&huart1);
+
+    // 停止DMA传输
+    HAL_UART_DMAStop(&huart1);
+
+    // 计算接收到的数据长度
+    rx1_length = sizeof(rx1_buffer) - __HAL_DMA_GET_COUNTER(&hdma_usart1_rx);
+
+    // 处理接收到的数据
+   
+			HAL_UART_Transmit_DMA(&huart3, rx1_buffer, rx1_length);
+              
+		rx1_flag =1;
+
+    // 重新启动DMA接收
+    HAL_UART_Receive_DMA(&huart1, rx1_buffer, sizeof(rx1_buffer));
+  }
+  /* USER CODE END USART1_IRQn 0 */
+  HAL_UART_IRQHandler(&huart1);
+  /* USER CODE BEGIN USART1_IRQn 1 */
+
+  /* USER CODE END USART1_IRQn 1 */
+}
+
+/**
+  * @brief This function handles USART3 global interrupt.
+  */
+void USART3_IRQHandler(void)
+{
+  /* USER CODE BEGIN USART3_IRQn 0 */
+	if(__HAL_UART_GET_FLAG(&huart3, UART_FLAG_IDLE) != RESET)
+  {
+    __HAL_UART_CLEAR_IDLEFLAG(&huart3);
+
+    // 停止DMA传输
+    HAL_UART_DMAStop(&huart3);
+
+    // 计算接收到的数据长度
+    rx3_length = sizeof(rx3_buffer) - __HAL_DMA_GET_COUNTER(&hdma_usart3_rx);
+
+    // 处理接收到的数据
+    for( i = 0; i < rx3_length; i++)
+    {
+      // 这里可以添加具体的数据处理逻辑
+    }
+		rx3_flag =1;
+
+    // 重新启动DMA接收
+    HAL_UART_Receive_DMA(&huart3, rx3_buffer, sizeof(rx3_buffer));//HAL库的统一的所有中断事件分发器,直接调用用户回调函数
+  }
+	
+  /* USER CODE END USART3_IRQn 0 */
+  HAL_UART_IRQHandler(&huart3);
+  /* USER CODE BEGIN USART3_IRQn 1 */
+
+  /* USER CODE END USART3_IRQn 1 */
+}
+
+/**
+  * @brief This function handles EXTI line[15:10] interrupts.
+  */
+void EXTI15_10_IRQHandler(void)
+{
+  /* USER CODE BEGIN EXTI15_10_IRQn 0 */
+
+  /* USER CODE END EXTI15_10_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_14);
+  /* USER CODE BEGIN EXTI15_10_IRQn 1 */
+
+  /* USER CODE END EXTI15_10_IRQn 1 */
+}
+
+/**
+  * @brief This function handles UART4 global interrupt.
+  */
+void UART4_IRQHandler(void)
+{
+  /* USER CODE BEGIN UART4_IRQn 0 */
+	if(__HAL_UART_GET_FLAG(&huart4, UART_FLAG_IDLE) != RESET)
+  {
+    __HAL_UART_CLEAR_IDLEFLAG(&huart4);
+    // 停止DMA传输
+    HAL_UART_DMAStop(&huart4);
+
+    // 计算接收到的数据长度
+    rx4_length = sizeof(rx4_buffer) - __HAL_DMA_GET_COUNTER(&hdma_uart4_rx);
+
+    // 处理接收到的数据
+   
+    
+    HAL_UART_Transmit_DMA(&huart3, rx4_buffer, rx4_length);
+    
+		rx4_flag =1;
+
+    // 重新启动DMA接收
+    HAL_UART_Receive_DMA(&huart4, rx4_buffer, sizeof(rx4_buffer));
+  }
+	
+  /* USER CODE END UART4_IRQn 0 */
+  HAL_UART_IRQHandler(&huart4);
+  /* USER CODE BEGIN UART4_IRQn 1 */
+
+  /* USER CODE END UART4_IRQn 1 */
+}
+
+/**
+  * @brief This function handles DMA2 stream2 global interrupt.
+  */
+void DMA2_Stream2_IRQHandler(void)
+{
+  /* USER CODE BEGIN DMA2_Stream2_IRQn 0 */
+
+  /* USER CODE END DMA2_Stream2_IRQn 0 */
+  HAL_DMA_IRQHandler(&hdma_usart1_rx);
+  /* USER CODE BEGIN DMA2_Stream2_IRQn 1 */
+
+  /* USER CODE END DMA2_Stream2_IRQn 1 */
+}
+
+/**
+  * @brief This function handles DMA2 stream7 global interrupt.
+  */
+void DMA2_Stream7_IRQHandler(void)
+{
+  /* USER CODE BEGIN DMA2_Stream7_IRQn 0 */
+
+  /* USER CODE END DMA2_Stream7_IRQn 0 */
+  HAL_DMA_IRQHandler(&hdma_usart1_tx);
+  /* USER CODE BEGIN DMA2_Stream7_IRQn 1 */
+
+  /* USER CODE END DMA2_Stream7_IRQn 1 */
+}
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
